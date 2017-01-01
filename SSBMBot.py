3949266b7cc17904d57a6d0c337837636b3c65f5
TheArcticKitten <noahortega58@gmail.com>
2016-12-31 21:53:56 -0600

    Commenting changes
    Changed format and now stores what comments it has commented on in a txt
    file.

--- SSBMBot.py
+++ SSBMBot.py
@@ -2,32 +2,36 @@
 import praw
 reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
 print(reddit.user.me())
-subreddit = reddit.subreddit("Kitten_Bots")
-already_commented = []	
+subreddit = reddit.subreddit("SSBM")
+f = open("commented.txt", "r+")
+counter = 0
 while(True):
 	for submission in subreddit.hot(limit=100):
 		submission.comments.replace_more(limit=0)
 		for comment in submission.comments.list():
-			if comment not in already_commented:
+			if comment.id not in open('commented.txt').read():
 				comment.body = comment.body.lower()
-				if "!doraki" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+				if ("!doraki" in comment.body) or ("!doraky" in comment.body):
+					counter +=1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
-							"**Doraki Instant Wall Jump**"
-							"\n\nWalljumping is performed by touching a wall, and then tapping away from it. However, it is also possible to wall jump directly from the ledge."
-							"\n\nThis technique is called the Ledge Walljump (or Doraki) and is performed as such."
-							"\n\n1. Grab the ledge"
-							"\n\n2. Drop from the ledge"
-							"\n\n3. Double jump back to the ledge, making sure that you rub up against the wall as you do so."
-							"\n\n4. While hanging from the ledge, tap away."
-							"\n\n(Credits to /u/ djloreddit)"
-							"\n\n[GIF](http://i.imgur.com/QxxmkgL.gif)"
-							"\n\n[SSBM Tutorials Video](https://www.youtube.com/watch?v=Ut-D3c4HfIg)"
-							"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
-							"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
-				if "!wavedash" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					"**Doraki Instant Wall Jump**"
+					"\n\nWalljumping is performed by touching a wall, and then tapping away from it. However, it is also possible to wall jump directly from the ledge."
+					"\n\nThis technique is called the Ledge Walljump (or Doraki) and is performed as such."
+					"\n\n1. Grab the ledge"
+					"\n\n2. Drop from the ledge"
+					"\n\n3. Double jump back to the ledge, making sure that you rub up against the wall as you do so."
+					"\n\n4. While hanging from the ledge, tap away."
+					"\n\n(Credits to /u/ djloreddit)"
+					"\n\n[GIF](http://i.imgur.com/QxxmkgL.gif)"
+					"\n\n[SSBM Tutorials Video](https://www.youtube.com/watch?v=Ut-D3c4HfIg)"
+					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
+					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
+					#already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+				if ("!wavedash" in comment.body) or ("!wavedashing" in comment.body):
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Wavedash**"
 					"\n\nA Wavedash is a technique that is performed by air dodging diagonally into the ground, causing the character to slide a short distance. It allows players to perform any ground action while moving horizontally on the ground."
@@ -36,9 +40,11 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/wavedash)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!waveshine" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Waveshine**"
 					"\n\nA wavedash can be performed anytime a character is on the ground and can jump. The shine can be canceled by jumping shortly after it is initiated. Therefore, it is possible for Fox and Flacoto jump out of a shine into a wavedash."
@@ -51,9 +57,11 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/Waveshine)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if ("!jcgrab" in comment.body) or ("!jumpcancelgrab" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Jump-Canceled Grab**"
 					"\n\nA jump-canceled grab is a technique where a character interrupts a dash or run with a jump, which itself is then jump-canceled with a grab during the jumpsquat animation. This results in the character using their standing grab while maintaining some momentum from their dash, the amount of which is based on their current speed and traction."
@@ -62,9 +70,11 @@ while(True):
 					"\n\n[Wiki](https://www.ssbwiki.com/Jump-canceled_grab)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
-				if "!lcancel" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
+				if ("!lcancel" in comment.body) or ("!lcanceling" in comment.body):
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**L-canceling**"
 					"\n\nL-canceling (an abbreviation of lag canceling or  is a technique that allows characters to act faster than usual when landing in the middle of an aerial attack."
@@ -74,9 +84,11 @@ while(True):
 					"\n\n[Wiki](https://www.ssbwiki.com/L-canceling)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if ("!dashdancing" in comment.body) or ("!dashdance" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Dash-Dancing**"
 					"\n\nDash-dancing is one of the most used techniques in Melee. While being one of the easiest to perform, it's one of the hardest to master. Dash-dancing is used to play spacing mindgames with the opponent, and to make openings that can punished."
@@ -86,9 +98,11 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/Dash-dance)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if ("!di" in comment.body) or ("!directionalinfluence" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Directional Influence**"
 					"\n\nDirectional influence, abbreviated DI, is the control the receiver of an attack has over his or her trajectory. DI can be used to alter, but not completely change, the original trajectory the player was sent at by an attack."
@@ -98,9 +112,11 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/Directional_influence)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!neutralgame" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Neutral Game**"
 					"\n\nThe neutral game, or just neutral, is a phase during gameplay when no player has a situational advantage over the other. In this phase, either player's objective is to \"win the neutral game\", i.e. land a hit with possible follow-ups, or punish the enemy for a failed attempt to do so."
@@ -108,9 +124,11 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/Neutral_game)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!scarjump" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Scar Jump**"
 					"\n\nThe Scar Jump is a variant of the Wall Jump in which a character wall jumps *without* using their midair jump from the ledge or stage."
@@ -119,9 +137,11 @@ while(True):
 					"\n\n[Wiki](https://www.ssbwiki.com/Scar_Jump)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if ("!sdi" in comment.body) or ("!smashdi" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Smash DI**"
 					"\n\nSmash directional influence is a mechanic that allows players to slightly alter their position during the freeze frames of being hit by an attack. Tapping the control stick in any direction during freeze frames will slightly move their character in that direction, allowing them to potentially escape multi-hit moves or certain combos"
@@ -130,8 +150,10 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/Smash_directional_influence)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
+					open('commented.txt', 'a').write(comment.id + "\n")
 				if ("!cc" in comment.body) or ("!crouchcanceling" in comment.body) or ("!ccing" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Crouch Canceling**"
 					"\n\nA crouch cancel (or CC) is a technique in the Super Smash Bros. series used to reduce the effect of an attack on the user. By crouching before getting hit by an attack, some aspect of the attack will be weakened."
@@ -139,9 +161,11 @@ while(True):
 					"\n\n[Wiki](https://www.ssbwiki.com/Crouch_cancel)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!moonwalk" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Moonwalking**"
 					"\n\nIn Super Smash Bros. Melee, the moonwalk is a technique that involves sliding backwards during in the initial dash animation"
@@ -151,9 +175,11 @@ while(True):
 					"\n\n[Wiki](https://www.ssbwiki.com/Moonwalk)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!shinostall" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Shino Stalling**"
 					"\n\nThe Shino stall grants the user total invincibility. This technique takes advantage of the invincibility caused by grabbing a ledge along with the invincibility caused by Sheik's Vanish move."
@@ -167,9 +193,11 @@ while(True):
 					"\n\n[SSBM Tutorials Video](https://www.youtube.com/watch?v=K3DI4Zl6b1E)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!shielddrop" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Shield Dropping**"
 					"\n\nShield platform dropping is an advanced technique in all four Super Smash Bros. games. It involves dropping through a soft platform while shielding by tilting the analog stick down."
@@ -178,9 +206,11 @@ while(True):
 					"\n\n[Wiki](http://www.ssbwiki.com/Shield_platform_dropping)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!marthkiller" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Marth Killer**"
 					"\n\nThe Marth Killer is a technique in Super Smash Bros. Melee that can be used to edge-hog a recovering Marth."
@@ -190,9 +220,11 @@ while(True):
 					"\n\n[Wiki](http://supersmashbros.wikia.com/wiki/Marth_Killer)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if ("!tech" in comment.body) or ("!teching" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Teching**"
 					"\n\nTeching allows the player to get back up after hitting a surface (such as a wall or floor) instead of lying on the ground until using a get-up action. This technique is great for survival as it can be used to survive combos, or walljump off the stage (wall-teching)"
@@ -202,9 +234,11 @@ while(True):
 					"\n\n[Wiki](https://www.ssbwiki.com/Tech)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!ecawk" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply(
 					"**Edge-canceled Aerial Wizard Kicks**"
 					"\n\nAn EcAWK is a technique in Project M for Ganondorf which allows for comboing, edgeguarding, and most importantly **STYLEEE**"
@@ -212,19 +246,36 @@ while(True):
 					"\n\n[Source](https://smashboards.com/threads/ganon-strategies-and-gameplay-discussion.347166/page-6#post-16997998)"
 					"\n\n[Github for bot](https://github.com/TheArcticKitten/SSBMBot)"
 					"\n\nAny problems or want to provide higher quality GIFs or tutorials? Message /u/ thearctickitten on reddit!")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
+				if "!dustscooting" in comment.body:
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
+					comment.reply("!wavedash")
+					open('commented.txt', 'a').write(comment.id + "\n")
+				if "!funcanceling" in comment.body:
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
+					comment.reply("!lcanceling")
+					open('commented.txt', 'a').write(comment.id + "\n")
 				if "!20xx" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply("The year is **20XX**. Everyone plays Fox at TAS levels of perfection. Because of this, the winner of a match depends solely on port priority. The RPS metagame has evolved to ridiculous levels due to it being the only remaining factor to decide matches.")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if "!jigglypuff" in comment.body:
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply("[Hey there](https://www.youtube.com/watch?v=wHn3FL9Drps)")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				if ("!ics" in comment.body) or ("!sad2king" in comment.body):
-					print("Replied to comment ID:" + comment.id)
+					counter += 1
+					print("Replied to comment ID:" + comment.id + " Count: " + counter)
 					comment.reply("[:c](https://youtu.be/1v2Nz9-iJ6g?t=71)")
-					already_commented.append(comment)
+					open('commented.txt', 'a').write(comment.id + "\n")
+					#already_commented.append(comment)
 				#if "" in comment.body:
 					#comment.reply(
 					#"****"
