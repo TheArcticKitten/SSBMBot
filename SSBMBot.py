62c07fab7d418f344be7d9fdf1886617b91f8f6c
TheArcticKitten <noahortega58@gmail.com>
2017-01-02 15:00:13 -0600

    Completed rewrite and fixed all bugs
    Also commented the code

--- SSBMBot.py
+++ SSBMBot.py
@@ -1,31 +1,32 @@
 #Authored by TheArcticKitten/Noah O.
 import praw
-import time
+from time import sleep
 import json
 import re
-reddit = praw.Reddit('bot1', user_agent='bot1 user agent')
+reddit = praw.Reddit('bot1', user_agent='bot1 user agent') #logs in to bot account from praw.ini
 subreddit = reddit.subreddit("Kitten_bots")
-replies = 0
- 
-print(reddit.user.me())
+replies = 0 #counter just to keep track of the replies
 
-with open("info.json") as f:
+print(reddit.user.me()) #just confirming that it logs in correctly under the right user
+
+with open("info.json") as f: #load in JSON file with all queries and responses
     info = json.load(f)
 
-while True:
-	for submission in subreddit.hot(limit=300):
+while True: #main loop to find command comments
+	for submission in subreddit.hot(limit=300): #loop through first 300 submissions in subreddit
 		submission.comments.replace_more(limit=0)
-		for comment in submission.comments.list():
-			if comment.id not in open('commented.txt').read():
-				for item in info:
-					if re.search("!{0}".format(item), comment.body, flags=re.IGNORECASE):
-						comment.reply("**{0}**\n\n{1}\n\n{2}\n\n{3}".format(
+		for comment in submission.comments.list(): #loop through all comments in each submission
+			if comment.id not in open('commented.txt').read(): #check if it has been replied to already by the bot
+				for item in info: #loop thorugh all queries
+					if re.search("!{0}".format(item), comment.body, flags=re.IGNORECASE): #check if any of the queries are in the comment body
+						comment.reply("**{0}**\n\n{1}\n\n{2}\n\n{3}".format( #reply the correct response for that specific query
 		                        info[item]['Name'],
 		                        "\n\n".join(info[item]['Description']),
 		                        "\n\n".join("[{0}]({1})".format(link, info[item]['Links'][link])
 		                        for link in info[item]['Links']),
-		                        "------\n[^SSBMBot](https://github.com/thearctickitten/SSBMBot) ^by ^[TheArcticKitten](/u/thearctickitten)"
+		                        "------\n[^SSBMBot](https://github.com/thearctickitten/SSBMBot) ^by ^[TheArcticKitten](/u/ thearctickitten)"
 		                    		))
-						open("commented.txt", "a+").write(comment.id + "\n")
-						replies += 1
-						print("Replied to comment ID: {0} Count: {1}".format(comment.id, replies))
+						open("commented.txt", "a+").write(comment.id + "\n") #append the comment id to the already commented txt file to prevent commenting on it again
+						replies += 1 #increment counter
+						print("Replied to comment ID: {0} Count: {1}".format(comment.id, replies)) #print out the id just as a log
+	time.sleep(30) #sleep for 30 seconds
